179127d7f8f25917dc68df5c91ef7771bc8adc13
Skaramal7 <skaramal7@gmail.com>
2026-02-20 11:41:55 -0300

    storage

--- storage/datastorage.c
+++ storage/datastorage.c
@@ -1,1 +1,38 @@
-#include "datastorage.h"
+#include"datastorage.h"
+
+#include <stdio.h>
+#include<stdlib.h>
+#include <string.h>
+
+ClientEntry* initStorage(int bucketsNum) {
+    ClientEntry* newArray = (ClientEntry*) malloc(sizeof(ClientEntry) * bucketsNum);
+    if (newArray == NULL) {
+        return NULL;
+    }
+
+    return newArray;
+}
+
+int hashClientId(char* clientId, int bucketsNum) {
+    int keyLength = strlen(clientId);
+    char* half1 = (char *) malloc(sizeof(char));
+    char* half2 = (char *) malloc(sizeof(char));
+
+    strncpy(half1, clientId, (keyLength / 2));
+    strncpy(half2, &clientId[(keyLength / 2)], keyLength);
+
+    int half1int, half2int;
+    sscanf(half1, "%d", &half1int);
+    sscanf(half2, "%d", &half2int);
+
+    int folded = half1int ^ half2int;
+    int index = folded % bucketsNum;
+
+    free(half1);
+    free(half2);
+    return index;
+}
+
+int main() {
+    int index = hashClientId("17769145414504", 200);
+}
--- storage/datastorage.h
+++ storage/datastorage.h
@@ -1,4 +1,33 @@
-#ifndef _IDEA_DATASTORAGE_H
-#define _IDEA_DATASTORAGE_H
+#ifndef DCACHE_DATASTORAGE_H
+#define DCACHE_DATASTORAGE_H
 
-#endif //_IDEA_DATASTORAGE_H
+struct objectNode {
+    char* key;
+    char* data;
+    struct objectNode* next;
+};
+typedef struct objectNode ObjectNode;
+
+struct clientEntry {
+    char* clientId;
+    ObjectNode* objectList;
+};
+typedef struct clientEntry ClientEntry;
+
+ClientEntry* initStorage(int bucketsNum);                                                         // returns empty array of ClientEntry ready for use
+
+int freeStorage(ClientEntry* clientArray);                                          // frees all storage on clientArray; returns 0 for success
+
+int insertClient(ClientEntry* clientArray, char* clientId);                         // inserts clientId in clientArray by hashing its ID; returns 0 for success, 2 if client already exists
+
+int removeClient(ClientEntry* clientArray, char* clientId);                         // removes clientId in clientArray
+
+int storeObject(ClientEntry* clientArray, char* clientId, char* key, char* data);   // stores new object with key in clientId; returns 0 for success, 2 if object already exists
+
+int deleteObject(ClientEntry* clientArray, char* clientId, char* key);              // deletes object with key from clientId's list
+
+int hashClientId(char* clientId, int bucketsNum);                                                     // returns hashed array position for given clientId
+
+char* buildClientId(char* ipaddr, char* portnum);                                   // returns given ip address and port number into a formatted id
+
+#endif
